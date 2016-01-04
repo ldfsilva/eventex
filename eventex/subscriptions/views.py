dd82e17e7f731f86283647c9b11a12760610d445
Leandro Ferreira da Silva <ldfsilva@gmail.com>
2016-01-03 21:22:19 -0600

    Realiza inscricao

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,7 +1,31 @@
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    context = {'form' : SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+
+        if form.is_valid():
+            body = render_to_string('subscriptions/subscription_email.txt',
+                                    form.cleaned_data)
+
+            mail.send_mail('[Eventex] Confirmação de Inscrição',
+                           body,
+                           'ldfsilva@gmail.com',
+                           ['ldfsilva@gmail.com', form.cleaned_data['email']])
+
+            messages.success(request, 'Inscrição realizada com sucesso!')
+
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'subscriptions/subscription_form.html',
+                          {'form': form})
+
+    else:
+        context = {'form' : SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
