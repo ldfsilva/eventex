f96a79a1e8329c7ba210be9c66277d1b49bfc568
Leandro Ferreira da Silva <ldfsilva@gmail.com>
2016-01-06 21:38:15 -0600

    Persiste a inscricao

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Leandro Silva',
+            cpf='12345678911',
+            email='ldfsilva@gmail.com',
+            phone='8179031476',
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
