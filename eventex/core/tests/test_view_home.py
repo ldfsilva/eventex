0a566e63b2bff8dce6c304a0ea0fc1b4b0ceb0d5
Leandro Ferreira da Silva <ldfsilva@gmail.com>
2016-01-21 19:09:56 -0600

    Speaker

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -3,6 +3,8 @@ from django.shortcuts import resolve_url as r
 
 
 class HomeTest(TestCase):
+    fixtures = ['keynotes.json']
+
     def setUp(self):
         self.response = self.client.get(r('home'))
 
@@ -18,11 +20,13 @@ class HomeTest(TestCase):
         expected = 'href="{}"'.format(r('subscriptions:new'))
         self.assertContains(self.response, expected)
 
-    def test_spekears(self):
+    def test_speakers(self):
         """Must show keynote speakers"""
         contents = [
+            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
             'Grace Hopper',
             'http://hbn.link/hopper-pic',
+            'href="{}"'.format(r('speaker_detail', slug='alan-turing')),
             'Alan Turing',
             'http://hbn.link/turing-pic',
         ]
